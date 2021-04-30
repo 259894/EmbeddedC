bd2e982ff3467553c208249d112918f346d4d04b
259894 <80876216+259894@users.noreply.github.com>
2021-04-30 21:15:06 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,15 +1,34 @@
+/**
+ * @file activity3.c
+ * @author Annappa M (annappa211@gmail.com)
+ * @brief Generate the PWM signals and visualise it in CRO
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #include <util/delay.h>
 
 #include "activity3.h"
 #include "activity2.h"
 
+/**
+ * @brief Function to initialize timer registers
+ * 
+ */
 void config_timer()
 {
      TCCR1A|=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
     TCCR1B|=(1<<CS11)|(1<<WGM12)|(1<<CS10);
     DDRB|=(1<<PB1);
 }
+
+/**
+ * @brief Function to produce duty cycle in PWM according to input ADC value
+ * 
+ */
 void pwm_output(void)
 {
 
